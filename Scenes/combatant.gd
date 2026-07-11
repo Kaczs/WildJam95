eb8100214965ff141fcccb9a719af4aa95d1712b
Taeze1000 <Taezeted@gmail.com>
2026-07-11 09:42:30 -0600

    added card draging

--- Scenes/combatant.gd
+++ Scenes/combatant.gd
@@ -1,5 +1,5 @@
 class_name Combatant 
-extends Node
+extends Control
 
 @export var max_health:int = 100
 ## If we want to "plant" a card targetting this enemy the next time we're here
@@ -13,11 +13,24 @@ var block:int # The amount of damage to be blocked
 func _ready() -> void:
 	current_health = max_health
 
+
 func take_damage(amount:int):
 	current_health -= amount
 	if current_health <= 0:
 		#die
 		self.queue_free()
 
+
 func gain_block(amount:int):
 	block += amount
+
+
+func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
+	if data is CardData:
+		return true
+	else:
+		return false
+
+
+func _drop_data(_at_position: Vector2, data: Variant) -> void:
+	CombatManager.play_card(data,self)
